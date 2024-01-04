dde46f37464f68f6192251f2fcd9993528147d8a
ABHISHEK DAS <abhishekdas300222210@gmail.com>
2024-01-04 13:35:11 +0530

    car in traffic simulation with sensors done

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const canvas = document.getElementById("canvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50); // x,y,width,height
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCentre(1), 100, 30, 50, "KEYS"); // x,y,width,height
+const traffic = [new Car(road.getLaneCentre(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
 
 function animate() {
-  car.update();
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
+
+  car.update(road.borders, traffic);
+
   canvas.height = window.innerHeight;
-  car.draw(ctx);
+
+  // camera
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+
+  road.draw(ctx);
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].draw(ctx, "red");
+  }
+  car.draw(ctx, "blue");
+
+  //camera
+  ctx.restore();
+
   requestAnimationFrame(animate);
 }
