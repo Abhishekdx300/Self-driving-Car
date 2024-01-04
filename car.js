1995e87582ace62dae7f456a03622c224acffb89
ABHISHEK DAS <abhishekdas300222210@gmail.com>
2024-01-04 21:35:49 +0530

    basic done

--- car.js
+++ car.js
@@ -117,7 +117,7 @@ class Car {
     this.y -= Math.cos(this.angle) * this.speed;
   }
 
-  draw(ctx, color) {
+  draw(ctx, color, drawSensor = false) {
     // ctx.save();
     // ctx.translate(this.x, this.y);
     // ctx.rotate(-this.angle);
@@ -143,6 +143,6 @@ class Car {
     }
 
     ctx.fill();
-    if (this.sensor) this.sensor.draw(ctx);
+    if (this.sensor && drawSensor) this.sensor.draw(ctx);
   }
 }
